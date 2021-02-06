586e3678b1e0b6f1544fa3adeaa8238763e11873
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2021-02-06 18:32:07 +0100

    modifications

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
 
+//Schema pour les sauces
 const sauceSchema = mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    imageUrl: { type: String, required: true },
+    id: { type: String, required: true },
     userId: { type: String, required: true },
-    price: { type: Number, required: true }
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, required: false, default: 0 },
+    dislikes: { type: Number, required: false, default: 0 },
+    usersLiked: { type: [String] },
+    usersDisliked: { type: [String] }
 })
 
 module.exports = mongoose.model('Sauce', sauceSchema)
--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
+//Schema pour les Users
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 })
 
-userSchema.plugin(uniqueValidator)
+userSchema.plugin(uniqueValidator) // On ajoute le mongoose-unique-validator au Schema
 
 module.exports = mongoose.model('User', userSchema)
