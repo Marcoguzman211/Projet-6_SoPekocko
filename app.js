e5640e911ff237f44de1f98ce6d37f1f54f25245
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2021-02-08 22:13:13 +0100

    Protection identifiants MongoDB

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const express = require('express')
+const express = require('express') //On importe express
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path')
+const helmet = require('helmet')
+const dotenv = require('dotenv') //.env pour sécuriser l'accès à la base de données
 
 const sauceRoutes = require('./routes/sauce')
 const userRoutes = require('./routes/user')
 
+dotenv.config() //Elle initie dotenv and rend les variables disponibles pour la connexion MongoDb
+
 //Connexion à la base de données
-mongoose.connect("mongodb+srv://pecockouser:ofT6aXhMgKne1dsr@cluster0.jyz8d.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.jyz8d.mongodb.net/SoPekocko?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(() => console.log('Connexion à MongoDB Atlas réussie !'))
     .catch(() => console.log('Connexion à MongoDB Atlas échouée !'));
 
-const app = express()
+const app = express() //Constante qui éxécute Express
+
+app.use(helmet()) //middleware Helmet pour les attaques XSS 
+
+//Ajout des Headers à la requête pour éviter le CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const http = require('http');
-const app = require('./app');
+const http = require('http'); //On initialise le http qui va attendre les requêtes et qui va y répondre
+const app = require('./app'); //Constante pour l'importation du app.js
 
+
+//Renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne 
 const normalizePort = val => {
     const port = parseInt(val, 10);
 
@@ -15,6 +17,7 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT ||  '3000');
 app.set('port', port);
 
+//Recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur 
 const errorHandler = error => {
     if (error.syscall !== 'listen') {
         throw error;
@@ -35,7 +38,7 @@ const errorHandler = error => {
     }
 };
 
-const server = http.createServer(app);
+const server = http.createServer(app); //Méthode pour créer le serveur et importe le app.js
 
 server.on('error', errorHandler);
 server.on('listening', () => {
